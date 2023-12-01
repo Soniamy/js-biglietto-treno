11a7ebb9dc91730927ea9bdc15aec3b0e7fad5b4
Soniamy <marianna.marzullo@gmail.com>
2023-12-01 18:11:44 +0100

    scomposizione problem

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
+/*
+1.Chiedere all'utente i km da fare
+2.Se il risultato è diverso da null && è un numero
+3. Fare il calcolo km
+  -altrimenti chiedere all'utente i km
+4.Chiedere età
+5.Se è un numero
+  -altrimenti chiedere età
+6.SE ha <18
+7.Allora fare il calcolo della variabile con il calcolo dei km e il -20%
+8. altrimenti se l'età è compresa tra +18 e 65
+9.Allora fare il calcolo della variabile senza sconto
+10.Se ha >= 65anni 
+11. Allora fare il calcolo della variabile con il calcolo dei km e il -40%
+12.Stampa variabile calcolo totale
+*/
+
 console.log('ciao')
