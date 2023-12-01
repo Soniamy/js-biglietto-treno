bcd06d17b65a868ae7e797fdd2e013722bb44d08
Soniamy <marianna.marzullo@gmail.com>
2023-12-01 21:58:34 +0100

    esecuzione del punto  6-7-8-9

--- src/js/main.js
+++ src/js/main.js
@@ -9,8 +9,8 @@
 6.SE ha <18
 7.Allora fare il calcolo della variabile con il calcolo dei km e il -20%
 8. altrimenti se l'età è compresa tra +18 e 65
-9.Allora fare il calcolo della variabile senza sconto
-10.Se ha >= 65anni 
+9.Se ha >= 65anni 
+10.Allora fare il calcolo della variabile senza sconto
 11. Allora fare il calcolo della variabile con il calcolo dei km e il -40%
 12.Stampa variabile calcolo totale
 */
@@ -20,6 +20,11 @@ const KM_PRICE = 0.21;
 if (km && !isNaN(km)) {
     let totCost = km * KM_PRICE;
     let eta = prompt('Scrivi la tua età');
+    if (eta < 18) {
+        totCost = totCost - totCost * 0.2;
+    } else if (eta >= 65) {
+        totCost = totCost - totCost * 0.4;
+    }
 } else {
     alert('Non hai inserito un km valido.');
 }
