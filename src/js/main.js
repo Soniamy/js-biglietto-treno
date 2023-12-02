8be6924ed8b19c9de8bce79052646f2b9a1bee29
Soniamy <marianna.marzullo@gmail.com>
2023-12-02 15:40:27 +0100

    aggiunta di NumberFormat e conclusione

--- src/js/main.js
+++ src/js/main.js
@@ -8,22 +8,32 @@
   -altrimenti chiedere età
 6.SE ha <18
 7.Allora fare il calcolo della variabile con il calcolo dei km e il -20%
-8. altrimenti se l'età è compresa tra +18 e 65
-9.Se ha >= 65anni 
-10.Allora fare il calcolo della variabile senza sconto
-11. Allora fare il calcolo della variabile con il calcolo dei km e il -40%
-12.Stampa variabile calcolo totale
+8.Se ha >= 65anni 
+9. Allora fare il calcolo della variabile con il calcolo dei km e il -40%
+10. altrimenti se l'età è compresa tra +18 e 65
+11.Allora fare il calcolo della variabile senza sconto
+12.Stampa variabile calcolo totale 
 */
-let km = prompt('Kilomatri che vuoi fare');
+let km = prompt('Kilometri che vuoi fare');
 const KM_PRICE = 0.21;
 
 if (km && !isNaN(km)) {
     let totCost = km * KM_PRICE;
     let eta = prompt('Scrivi la tua età');
-    if (eta < 18) {
-        totCost = totCost - totCost * 0.2;
-    } else if (eta >= 65) {
-        totCost = totCost - totCost * 0.4;
+    if (eta && !isNaN(eta)) {
+        if (eta < 18) {
+            totCost = totCost - totCost * 0.2;
+        } else if (eta >= 65) {
+            totCost = totCost - totCost * 0.4;
+        }
+        let numberFormat = new Intl.NumberFormat('it-IT', {
+            style: 'currency',
+            currency: 'EUR',
+        });
+        document.getElementById('price').innerHTML =
+            numberFormat.format(totCost);
+    } else {
+        alert('Non hai inserito il carattere numerico corretto');
     }
 } else {
     alert('Non hai inserito un km valido.');
